b754887b045f9f1db511b8ee0bf6beab38a4838f
Volker Esser <volker@esser-work.de>
2025-02-22 11:53:46 +0100

    feat: enhance ConfigurationManagerBuilder and FusionAppBuilder with detailed JSDoc comments and improve build methods

--- packages/fusion-kit/src/builder/ConfigurationManagerBuilder.ts
+++ packages/fusion-kit/src/builder/ConfigurationManagerBuilder.ts
@@ -1,19 +1,38 @@
 import { ConfigurationManager } from '..';
 
+/**
+ * Builder class for creating a ConfigurationManager instance.
+ */
 export class ConfigurationManagerBuilder {
   private configurationDirectory: string = '';
   private filesToLoad: { filename: string; id: string }[] = [];
 
+  /**
+   * Sets the configuration directory.
+   * @param directory - The directory where configuration files are located.
+   * @returns The current instance of ConfigurationManagerBuilder.
+   */
   withConfigurationDirectory(directory: string): ConfigurationManagerBuilder {
     this.configurationDirectory = directory;
     return this;
   }
 
+  /**
+   * Adds a file to the list of files to load.
+   * @param filename - The name of the file to load.
+   * @param id - The identifier for the file content.
+   * @returns The current instance of ConfigurationManagerBuilder.
+   */
   withFileToLoad(filename: string, id: string): ConfigurationManagerBuilder {
     this.filesToLoad.push({ filename, id: id });
     return this;
   }
 
+  /**
+   * Builds and returns an instance of ConfigurationManager.
+   * @returns A promise that resolves to an instance of ConfigurationManager.
+   * @throws Error if the configuration directory is not set.
+   */
   async build(): Promise<ConfigurationManager> {
     if (!this.configurationDirectory) {
       throw new Error('Configuration directory is required to build ConfigurationManager');
@@ -21,9 +40,12 @@ export class ConfigurationManagerBuilder {
 
     const configManager = new ConfigurationManager(this.configurationDirectory);
 
-    for (const file of this.filesToLoad) {
-      await configManager.loadJsonContent(file.filename, file.id);
-    }
+    // Load all files concurrently
+    await Promise.all(
+      this.filesToLoad.map(async file => {
+        await configManager.loadJsonContent(file.filename, file.id);
+      }),
+    );
 
     return configManager;
   }
--- packages/fusion-kit/src/builder/FusionAppBuilder.ts
+++ packages/fusion-kit/src/builder/FusionAppBuilder.ts
@@ -2,6 +2,9 @@ import { AuthService, Logger, RemoteModuleManager } from 'fusion-kit-contracts';
 import { ConfigurationManager, FusionApp } from '..';
 import { EncryptedStorage } from '../core/EncryptedStorage';
 
+/**
+ * Builder class for creating a FusionApp instance.
+ */
 export class FusionAppBuilder {
   private name: string = '';
   private authServiceFactory: (() => Promise<AuthService>) | undefined;
@@ -10,47 +13,87 @@ export class FusionAppBuilder {
   private encryptedStorage: EncryptedStorage | undefined;
   private remoteModuleManager: RemoteModuleManager | undefined;
 
+  /**
+   * Sets the name of the FusionApp.
+   * @param name - The name of the application.
+   * @returns The current instance of FusionAppBuilder.
+   */
   withName(name: string): FusionAppBuilder {
     this.name = name;
     return this;
   }
 
+  /**
+   * Sets the authentication service factory.
+   * @param authServiceFactory - A factory function that returns a promise of AuthService.
+   * @returns The current instance of FusionAppBuilder.
+   */
   withAuthFactory(authServiceFactory: () => Promise<AuthService>): FusionAppBuilder {
     this.authServiceFactory = authServiceFactory;
     return this;
   }
 
+  /**
+   * Sets the configuration manager.
+   * @param configManager - An instance of ConfigurationManager.
+   * @returns The current instance of FusionAppBuilder.
+   */
   withConfigManager(configManager: ConfigurationManager): FusionAppBuilder {
     this.configManager = configManager;
     return this;
   }
 
+  /**
+   * Sets the logger.
+   * @param logger - An instance of Logger.
+   * @returns The current instance of FusionAppBuilder.
+   */
   withLogger(logger: Logger): FusionAppBuilder {
     this.logger = logger;
     return this;
   }
 
+  /**
+   * Sets the encrypted storage.
+   * @param encryptedStorage - An instance of EncryptedStorage.
+   * @returns The current instance of FusionAppBuilder.
+   */
   withEncryptedStorage(encryptedStorage: EncryptedStorage): FusionAppBuilder {
     this.encryptedStorage = encryptedStorage;
     return this;
   }
 
+  /**
+   * Sets the remote module manager.
+   * @param remoteModuleManager - An instance of RemoteModuleManager.
+   * @returns The current instance of FusionAppBuilder.
+   */
   withRemoteModuleManager(remoteModuleManager: RemoteModuleManager): FusionAppBuilder {
     this.remoteModuleManager = remoteModuleManager;
     return this;
   }
 
+  /**
+   * Builds and returns an instance of FusionApp.
+   * @returns A promise that resolves to an instance of FusionApp.
+   * @throws Error if required properties are missing.
+   */
   async build(): Promise<FusionApp> {
     if (!this.name || !this.authServiceFactory) {
-      throw new Error('Missing required properties to build ShellApp');
+      throw new Error('Missing required properties to build FusionApp');
     }
 
     const authService = await this.authServiceFactory();
     const app = new FusionApp(this.name, authService);
-    if (this.configManager) app.configurationManager = this.configManager;
-    if (this.logger) app.logger = this.logger;
-    if (this.encryptedStorage) app.encryptedStorage = this.encryptedStorage;
-    if (this.remoteModuleManager) app.remoteModuleManager = this.remoteModuleManager;
+
+    // Assign optional properties if they are defined
+    Object.assign(app, {
+      configurationManager: this.configManager,
+      logger: this.logger,
+      encryptedStorage: this.encryptedStorage,
+      remoteModuleManager: this.remoteModuleManager,
+    });
+
     return app;
   }
 }
