895241b534996a66daae2c2b07707253319d31d9
Volker Esser <volker@esser-work.de>
2025-02-22 16:42:51 +0100

    refactor: streamline App.vue and MicroFrontendWrapper.vue for improved readability and maintainability

--- packages/app-shell/src/App.vue
+++ packages/app-shell/src/App.vue
@@ -2,12 +2,7 @@
   <div class="shell-container">
     <div class="toolbar">
       <button @click="navigateToHome">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          width="24"
-          height="24"
-        >
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
           <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
         </svg>
       </button>
@@ -15,72 +10,88 @@
       <button @click="handleShowNotification">Show Notification</button>
       <button @click="handleShowToast">Show Toast</button>
       <button @click="handleLogout">
-        <svg
-          class="icon"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          width="24"
-          height="24"
-        >
-          <path
-            d="M16 13v-2H7V8l-5 4 5 4v-3h9zm4-9H10v2h10v14H10v2h10c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2z"
-          />
+        <svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+          <path d="M16 13v-2H7V8l-5 4 5 4v-3h9zm4-9H10v2h10v14H10v2h10c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2z" />
         </svg>
       </button>
     </div>
 
-    <router-view ></router-view>
+    <router-view></router-view>
 
     <LoggerDisplay class="logger"></LoggerDisplay>
   </div>
 </template>
 
 <script setup lang="ts">
-import LoggerDisplay from "./components/LoggerDisplay.vue";
-import { inject } from "vue";
-import type { FusionApp } from "fusion-kit";
-import { AppFrameAdapter } from "./utils/AppFrameAdapter";
-import { useRouter } from "vue-router";
+import LoggerDisplay from './components/LoggerDisplay.vue';
+import { inject, onMounted } from 'vue';
+import type { FusionApp } from 'fusion-kit';
+import { AppFrameAdapter } from './utils/AppFrameAdapter';
+import { useRouter } from 'vue-router';
+import type { RemoteModuleConfiguration } from 'fusion-kit-contracts';
 const router = useRouter();
 
 // Inject the shellApp provided in main.ts
-const shellApp = inject<FusionApp>("fusionApp");
+const fusionApp = inject<FusionApp>('fusionApp');
 
-if (!shellApp) {
-  throw new Error("shellApp not found");
+if (!fusionApp) {
+  throw new Error('shellApp not found');
 }
 //register the frame adapter
-shellApp.registerFrameAdapter(new AppFrameAdapter());
+fusionApp.registerFrameAdapter(new AppFrameAdapter());
 
 const navigateToHome = () => {
-  router.push("/");
+  router.push('/');
 };
 
 const handleLogout = async () => {
-  await shellApp.auth.logout();
-  console.log("Logged out");
+  await fusionApp.auth.logout();
+  console.log('Logged out');
 };
 
 const handleShowMessageBox = () => {
-  shellApp.userFeedback.showMessageBox(
-    "This is a message box",
-    [{ message: "Hello from Shell App", type: "info" }],
-    "cancel",
-    "ok"
-  );
+  fusionApp.userFeedback.showMessageBox('This is a message box', [{ message: 'Hello from Shell App', type: 'info' }], 'cancel', 'ok');
 };
 
 const handleShowNotification = () => {
-  shellApp.userFeedback.showNotification("Hello from Shell App", "info");
+  fusionApp.userFeedback.showNotification('Hello from Shell App', 'info');
 };
 
 const handleShowToast = () => {
-  shellApp.userFeedback.showToast("Hello from Shell App", "info");
+  fusionApp.userFeedback.showToast('Hello from Shell App', 'info');
 };
+
+// Load microfrontends on component mount
+onMounted(async () => {
+  if (!fusionApp) {
+    console.error('Fusion App is not initialized');
+    return;
+  }
+
+  if (!fusionApp.remoteModuleManager) {
+    console.error('Remote Module Manager is not initialized');
+    return;
+  }
+
+  if (!fusionApp.configurationManager) {
+    console.error('Configuration Manager is not initialized');
+    return;
+  }
+  //load modules from configuration
+  const dynamicRemotes = fusionApp.configurationManager.getContent<RemoteModuleConfiguration[]>('dynamicRemotes');
+
+  if (!dynamicRemotes) {
+    console.error('Dynamic Remotes is not initialized');
+    return;
+  }
+
+  // TODO - check if module is already loaded
+  // initialize remote modules.
+  await fusionApp.remoteModuleManager.loadRemoteModules(dynamicRemotes);
+});
 </script>
 
 <style scoped>
-
 .shell-container {
   display: grid;
   grid-template-rows: auto 1fr auto;
--- packages/app-shell/src/views/MicroFrontendWrapper.vue
+++ packages/app-shell/src/views/MicroFrontendWrapper.vue
@@ -4,7 +4,7 @@
 
 <script setup lang="ts">
 import type { FusionApp } from 'fusion-kit';
-import type { Module, RemoteModuleConfiguration } from 'fusion-kit-contracts';
+import type { Module } from 'fusion-kit-contracts';
 import { inject, defineProps, onMounted, onUnmounted } from 'vue';
 
 const fusionApp = inject<FusionApp>('fusionApp');
@@ -16,8 +16,6 @@ const props = defineProps<{
 let module: Module | undefined = undefined;
 
 onMounted(async () => {
-  //initialize remoteModuleManager
-  await initialize();
   //load module
   loadModule();
 
@@ -44,34 +42,6 @@ onUnmounted(() => {
   //reset value
   module = undefined;
 });
-
-const initialize = async () => {
-  if (!fusionApp) {
-    console.error('Fusion App is not initialized');
-    return;
-  }
-
-  if (!fusionApp.remoteModuleManager) {
-    console.error('Remote Module Manager is not initialized');
-    return;
-  }
-
-  if (!fusionApp.configurationManager) {
-    console.error('Configuration Manager is not initialized');
-    return;
-  }
-  //load modules from configuration
-  const dynamicRemotes = fusionApp.configurationManager.getContent<RemoteModuleConfiguration[]>('dynamicRemotes');
-
-  if (!dynamicRemotes) {
-    console.error('Dynamic Remotes is not initialized');
-    return;
-  }
-
-  // TODO - check if module is already loaded
-  // initialize remote modules.
-  await fusionApp.remoteModuleManager.loadRemoteModules(dynamicRemotes);
-};
 
 const loadModule = () => {
   if (module) return;
