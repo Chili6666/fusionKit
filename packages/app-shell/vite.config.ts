fd155a4b10601e5f7bc141721a831d4a8520f561
Volker Esser <volker@esser-work.de>
2025-02-27 08:06:11 +0100

    refactor: simplify remote configuration in remotes.json and update Vite config

--- packages/app-shell/vite.config.ts
+++ packages/app-shell/vite.config.ts
@@ -4,17 +4,13 @@ import federation from '@originjs/vite-plugin-federation';
 
 import remotes from './public/config/remotes.json';
 
-const getFormattedRemotes = () => {
-  return Object.fromEntries(Object.entries(remotes).map(([key, value]) => [key, value.url]));
-};
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     federation({
       name: 'app-shell',
-      remotes: getFormattedRemotes(),
+      remotes: remotes,
       shared: ['vue'],
     }),
   ],
